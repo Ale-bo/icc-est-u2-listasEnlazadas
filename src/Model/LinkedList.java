264b23f0f6371aa9c813927e49365d47676c0af1
SebasLzCb <sebas2006loza@gmail.com>
2025-06-10 19:31:57 -0500

    implementacion de MVC con respectivas clases, por el momento ya listo Model

---
+++ src/Model/LinkedList.java
@@ -0,0 +1,82 @@
+package Model;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void appendToTail(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        size++;
+    }
+
+    public Node<T> findByValue(T value) {
+        Node<T> current = head;
+        while (current != null) {
+            if (current.getValue().equals(value)) {
+                return current;
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
+
+    public void deleteByValue(T value) {
+        if (head == null) return;
+
+        if (head.getValue().equals(value)) {
+            head = head.getNext();
+            size--;
+            return;
+        }
+
+        Node<T> current = head;
+        while (current.getNext() != null && !current.getNext().getValue().equals(value)) {
+            current = current.getNext();
+        }
+
+        if (current.getNext() != null) {
+            current.setNext(current.getNext().getNext());
+            size--;
+        }
+    }
+
+    public void print () {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.getValue() + " -> ");
+            current = current.getNext();
+        }
+        System.out.println("null");
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+}
